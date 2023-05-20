ad979c7904e6402e068089d43628802a8c1d2db5
Eric Mugo <mugoericpg20@gmail.com>
2023-05-20 06:30:33 +0300

    Added sleep method

--- countdown.rb
+++ countdown.rb
@@ -12,3 +12,18 @@ def countdown(number)
 
 
 end
+
+def countdown_with_sleep(number)
+    while number > 0
+        puts "#{number} SECOND(S)!" 
+        sleep(1) # Pauses for one second
+        number -= 1
+    end
+
+    "HAPPY NEW YEAR!"
+        
+
+    
+
+
+end
